d58eb1867fea8c20b2079554e7b498c3864ec5ac
Merdi-kim <merdikimuha1@gmail.com>
2022-05-09 04:22:15 +0300

    set home page

--- pages/store/[id].js
+++ pages/store/[id].js
@@ -3,9 +3,21 @@ import ItemCard from '../../components/ItemCard'
 
 function Store() {
   return (
-    <div>
-        <nav></nav>
-        <div>
+    <div className={styles.store}>
+        <nav>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6VsHnK9L_CT0G0x9NyasJ57zTXbJ0igcC2Q&usqp=CAU" alt="logo" />
+          <span>TMP electronics</span>
+        </nav>
+        <div className={styles.itemsList}>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
             <ItemCard/>
         </div>
     </div>
